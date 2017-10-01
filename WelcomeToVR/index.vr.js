18f3fe0107e2b97112806c4b84aae605aea33b9f
Kit Work Desktop <kit@rushdigital.co.nz>
2017-10-02 11:56:20 +1300

    updated vr stuffs

--- WelcomeToVR/index.vr.js
+++ WelcomeToVR/index.vr.js
@@ -1,49 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   AppRegistry,
   asset,
+  Animated,
+  VrAnimated,
   Pano,
   Text,
   View,
+  Model,
+  StyleSheet
 } from 'react-vr';
 
-export default class WelcomeToVR extends React.Component {
-  render() {
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontSize: 0.4
+  },
+  red: {
+    color: 'red'
+  }
+});
+
+class OOLALA extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bounceValue: new Animated.Value(0),
+      textColor: 'white'
+    };
+  };
+
+  render(){
     return (
       <View>
         <Pano source={asset('ollalie-8505-Panorama-8k.jpg')}/>
-        <Text
+        <Animated.Image
+          source={{uri: 'http://i.imgur.com/XMKOH81.jpg'}}
           style={{
-            backgroundColor: 'white',
-            color: 'black',
-            fontSize: 0.7,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          RUSH
-        </Text>
-        <Text
-          style={{
-            fontSize: 0.7,
-            color: 'white',
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          DIGITAL
-        </Text>
-      </View>
+            flex: 1,
+            width: 1,
+            height: 1,
+            transform: [  
+              {translate: [-1,1,-5]},                      // `transform` is an ordered array
+              {scale: this.state.bounceValue},  // Map `bounceValue` to `scale`
+            ]
+          }}
+        />
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          width: 2,
+          alignItems: 'stretch',
+          transform: [{translate: [-1,1,-5]}],
+        }}>
+        
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red'}}>
+            <Text 
+            style={{color: this.state.textColor, fontSize: 0.2, textAlign: 'center'}}
+            onEnter={()=>{
+              this.setState({textColor:'red'});
+              }}
+            onExit={()=>{
+              this.setState({textColor:'white'});
+              }}>
+              Red
+            </Text>
+          </View>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'orange'}}>
+            <Text style={{fontSize: 0.2, textAlign: 'center'}}>Orange</Text>
+          </View>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'yellow'}}>
+            <Text style={{fontSize: 0.2, textAlign: 'center'}}>Yellow</Text>
+          </View>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'green'}}>
+            <Text style={[styles.bigblue, {color:'green'}]}>Green</Text>
+          </View>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'blue'}}>
+            <Text style={styles.bigblue}>Blue</Text>
+          </View>
+        </View>
+      </View> 
     );
-  }
-};
+  };
 
-AppRegistry.registerComponent('WelcomeToVR', () => WelcomeToVR);
+  componentDidMount() {
+    this.state.bounceValue.setValue(1.5); // start large
+    Animated.spring(
+      this.state.bounceValue,
+      {
+        toValue: 0.8,
+        friction: 1
+      }
+    ).start();
+  };
+}
+
+AppRegistry.registerComponent("app", () => OOLALA);
